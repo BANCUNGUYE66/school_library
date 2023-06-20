ac506277c5595dad38c2a462370a93b5d028af2b
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-06-20 22:52:51 +0300

    fix linters

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,6 @@ require_relative 'decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     trimmed_name = @nameable.correct_name[0...10]
-    trimmed_name.length < @nameable.correct_name.length ? "#{trimmed_name}..." : trimmed_name
+    trimmed_name.length < @nameable.correct_name.length ? trimmed_name.to_s : trimmed_name
   end
 end
